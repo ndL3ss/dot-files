24a9c103baa901926c2b8ed44adb9361b827f749
Haren K <harenreddykondepalli@proton.me>
2025-06-27 23:50:42 +0530

    Added i3, picom, zshrc, nvim and kitty configs to git

---
+++ .config/nvim/init.lua
@@ -0,0 +1,21 @@
+--bootstrap lazy.nvim
+--looks to see if lazy.nvim exists and git clones it if not
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+   { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+--till here
+
+require("vim-options")
+require("lazy").setup("plugins")
